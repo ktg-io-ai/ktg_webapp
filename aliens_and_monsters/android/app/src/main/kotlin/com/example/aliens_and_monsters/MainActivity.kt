9f80f5cf07b6a374b625758c2fdb57a277f470ac
ktg-io-ai <rene.reyes.alquimi@gmail.com>
2025-05-10 07:38:46 -0400

    Full Commit

--- aliens_and_monsters/android/app/src/main/kotlin/com/example/aliens_and_monsters/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.aliens_and_monsters
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- masayoshi_son_challenge/start_server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %~dp0
-node server.js
-pause
