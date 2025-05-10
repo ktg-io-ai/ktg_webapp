c5108c8382aa5557ddf6f4b267fd425e0233156d
ktg-io-ai <rene.reyes.alquimi@gmail.com>
2025-05-10 07:38:46 -0400

    Full Commit

--- backend/karma-backend-v2-main/bucket/route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const { viewPrivateMedia } = require('../controllers/media')
-const router = express.Router()
-
-router.route('/media/:mediaToken').get(viewPrivateMedia)
-
-module.exports = router
